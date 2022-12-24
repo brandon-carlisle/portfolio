43f34cd51324ca00a830598b9e389a03a85b3468
Brandon <93879641+beanziii@users.noreply.github.com>
2022-12-24 23:03:25 +0000

    fixed tailwind config

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-black">
+      <body>
         <Main />
         <NextScript />
       </body>
