2d6650d47194171e959f1b65ecd18a3ba27cf4ff
Brandon <93879641+beanziii@users.noreply.github.com>
2022-12-25 22:55:29 +0000

    added homepage tagline

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 import Head from 'next/head';
-import Shapes from '../components/shapes';
 
 function Home() {
   return (
     <>
       <Head>
         <title>Brandon Carlisle | Web Developer</title>
+
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <section className="relative mx-auto">
+      <section className="relative">
         <header>
-          <h1 className="font-semi-bold text-5xl leading-tight">
-            A web developer interested in modern tooling and styling using
-            React, Next and Tailwind.
+          <h1 className="font-semi-bold text-6xl leading-tight">
+            A front end web developer interested in modern tools, and making
+            cool stuff on the web.
           </h1>
         </header>
       </section>
