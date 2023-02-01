6520118b0a71ea2e3a960894f1dc251434b64093
Brandon <93879641+beanziii@users.noreply.github.com>
2023-02-01 18:47:06 +0000

    removed pointless stuff

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,15 +2,10 @@ function Contact() {
   return (
     <div className="mb-8 flex flex-col gap-2">
       <h2 className="mb-2">Feel free to reach out to me via</h2>
-      <div className="flex gap-1">
-        <a href="mailto:brandoncarlisledev@gmail.com">Email</a>
-      </div>
-
-      <div className="flex gap-1">
-        <a href="https://github.com/beanziii" target="_blank" rel="noreferrer">
-          GitHub
-        </a>
-      </div>
+      <a href="mailto:brandon@carlisle.dev">Email</a>
+      <a href="https://github.com/beanziii" target="_blank" rel="noreferrer">
+        GitHub
+      </a>
     </div>
   );
 }
