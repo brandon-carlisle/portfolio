436c52a61f7571ec84a9c0a18eebde48a4e4487f
Brandon <93879641+beanziii@users.noreply.github.com>
2023-02-08 18:56:45 +0000

    active link now highlighted in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,28 +1,38 @@
+'use client';
+
+import clsx from 'clsx';
 import Link from 'next/link';
+import { useSelectedLayoutSegments } from 'next/navigation';
+
+const menu = [
+  { title: 'About', pathname: '/about' },
+  { title: 'Projects', pathname: '/projects' },
+  { title: 'Blog', pathname: '/blog' },
+];
 
 function Navbar() {
+  const segments = useSelectedLayoutSegments();
+
   return (
-    <nav className="mx-auto flex max-w-screen-md items-center justify-between pt-2 text-lg">
-      <Link href="/" className="font-normal">
+    <nav className="mx-auto flex max-w-screen-md items-center justify-between text-lg">
+      <Link href="/" className="tracking-wide py-2">
         Brandon Carlisle
       </Link>
 
-      <ul className="flex gap-3">
-        <li>
-          <Link href="/about" className="font-normal">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link href="/projects" className="font-normal">
-            Projects
-          </Link>
-        </li>
-        <li>
-          <Link href="/blog" className="font-normal">
-            Blog
-          </Link>
-        </li>
+      <ul className="flex gap-2">
+        {menu.map((item) => (
+          <li key={item.pathname}>
+            <Link
+              className={clsx(
+                'font-normal hover:bg-blue-900/40 transition-all py-2 px-4 rounded-md',
+                item.title.toLowerCase() === segments[0] && 'bg-blue-900/30'
+              )}
+              href={item.pathname}
+            >
+              {item.title}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
