daaf085be0edaa05ea39029e9945d9c99b2cbb39
beanziii <93879641+beanziii@users.noreply.github.com>
2023-02-13 22:55:21 +0000

    added meta desc

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,10 @@ import DefaultTags from '../components/DefaultTags';
 export default function Head() {
   return (
     <>
+      <meta
+        name="description"
+        content="Brandon Carlisle | Web developer interested in modern tools, and making cool stuff on the web"
+      />
       <DefaultTags />
     </>
   );
