390fb8e25d06e965ff7c532a28c79f26894064d4
beanziii <93879641+beanziii@users.noreply.github.com>
2023-02-13 22:56:43 +0000

    added meta desc to default tags

--- app/head.tsx
+++ app/head.tsx
@@ -3,10 +3,6 @@ import DefaultTags from '../components/DefaultTags';
 export default function Head() {
   return (
     <>
-      <meta
-        name="description"
-        content="Brandon Carlisle | Web developer interested in modern tools, and making cool stuff on the web"
-      />
       <DefaultTags />
     </>
   );
--- components/DefaultTags.tsx
+++ components/DefaultTags.tsx
@@ -2,6 +2,10 @@ export default function DefaultTags({ title }: { title?: string }) {
   return (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="description"
+        content="Brandon Carlisle | Web developer interested in modern tools, and making cool stuff on the web"
+      />
       <link href="/favicon.ico" rel="shortcut icon" />
       <title>
         {title ? `${title} | Brandon Carlisle` : 'Brandon Carlisle'}
