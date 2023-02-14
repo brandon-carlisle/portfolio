469acbfe835d427cc09859dcb2ea30fc6968cfc1
beanziii <93879641+beanziii@users.noreply.github.com>
2023-02-14 14:36:19 +0000

    fix contact component overflow

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,31 +5,29 @@ export default function Contact() {
   return (
     <Section>
       <Container>
-        <div className="grid grid-cols-2 items-center">
-          <p className="text-xl font-semibold leading-tight sm:text-4xl">
-            Get in touch
+        <p className="mb-4 text-xl font-semibold leading-tight sm:text-4xl">
+          Get in touch
+        </p>
+        <div className="flex flex-col gap-2">
+          <p className="text-lg font-light">
+            You can email me at{' '}
+            <a
+              className="font-semibold underline"
+              href="mailto:brandon@carlisle.dev"
+            >
+              brandon@carlisle.dev
+            </a>{' '}
+            or you can view my{' '}
+            <a
+              className="font-semibold underline"
+              href="https://github.com/brandon-carlisle"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>{' '}
+            to reach out there.
           </p>
-          <div className="flex flex-col gap-2">
-            <p className="text-lg font-light">
-              You can email me at{' '}
-              <a
-                className="font-semibold underline"
-                href="mailto:brandon@carlisle.dev"
-              >
-                brandon@carlisle.dev
-              </a>{' '}
-              or you can view my{' '}
-              <a
-                className="font-semibold underline"
-                href="https://github.com/brandon-carlisle"
-                target="_blank"
-                rel="noreferrer"
-              >
-                GitHub
-              </a>{' '}
-              to reach out there.
-            </p>
-          </div>
         </div>
       </Container>
     </Section>
