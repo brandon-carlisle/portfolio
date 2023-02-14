72cca220aff144bffade74251fea3ba1bb897f93
beanziii <93879641+beanziii@users.noreply.github.com>
2023-02-14 13:56:15 +0000

    adding webhook to trigger on demand ISR

---
+++ pages/api/revalidate.tsx
@@ -0,0 +1,46 @@
+import { isValidRequest } from '@sanity/webhook';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  message: string;
+};
+
+const secret = process.env.SANITY_WEBHOOK_SECRET!;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method !== 'POST') {
+    console.error('Must be a POST request');
+    return res.status(401).json({ message: 'Must be a POST request' });
+  }
+
+  if (!isValidRequest(req, secret)) {
+    res.status(401).json({ message: 'Invalid signature' });
+    return;
+  }
+
+  try {
+    const {
+      body: { type, slug },
+    } = req;
+
+    switch (type) {
+      case 'post':
+        await res.revalidate(`/blog/${slug}`);
+        return res.json({
+          message: `Revalidated "${type}" with slug "${slug}"`,
+        });
+      case 'project':
+        await res.revalidate(`/project/${slug}`);
+        return res.json({
+          message: `Revalidated "${type}" with slug "${slug}"`,
+        });
+    }
+
+    return res.json({ message: 'No managed type' });
+  } catch (err) {
+    return res.status(500).send({ message: 'Error revalidating' });
+  }
+}
