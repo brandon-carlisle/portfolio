8702a701f0475d91aa2d09630bd6cc10f797ca24
beanziii <93879641+beanziii@users.noreply.github.com>
2023-02-14 14:54:00 +0000

    divider had wrong class

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -5,7 +5,7 @@ type DividerProps = {
 
 export default function Divider({ childLeft, childRight }: DividerProps) {
   return (
-    <div className="mb-12 flex items-center justify-evenly gap-2 text-gray-200/50">
+    <div className="mb-12 flex items-center justify-between gap-2 text-gray-200/50">
       {childLeft}
       <div className="h-[1px] w-1/4 bg-blue-500/20 md:w-1/2"></div>
       {childRight}
