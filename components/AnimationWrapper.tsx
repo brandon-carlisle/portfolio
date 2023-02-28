917ff3d9cc2a24d21a57c3c3962c62b82af4ec45
beanziii <93879641+beanziii@users.noreply.github.com>
2023-02-28 18:25:25 +0000

    adding key

--- components/AnimationWrapper.tsx
+++ components/AnimationWrapper.tsx
@@ -1,15 +1,19 @@
 'use client';
 
 import { motion, AnimatePresence } from 'framer-motion';
+import { usePathname } from 'next/navigation';
 
 interface AnimationWrapperProps {
   children: React.ReactNode;
 }
 
 export default function AnimationWrapper({ children }: AnimationWrapperProps) {
+  const pathname = usePathname();
+
   return (
     <AnimatePresence mode="wait">
       <motion.div
+        key={pathname}
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: 20 }}
