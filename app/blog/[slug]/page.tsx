f198a8113e7ee992699641ee6a92d87ecdf1e49b
beanziii <93879641+beanziii@users.noreply.github.com>
2023-03-11 23:51:14 +0000

    working dynamic metadata

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -6,10 +6,28 @@ import type { BlogPostData } from '../page';
 import { groq } from 'next-sanity';
 import Divider from '../../../components/Divider';
 import { notFound } from 'next/navigation';
+import { type Metadata } from 'next';
 
 export const revalidate = 60;
 
+type BlogPostParams = {
+  params: {
+    slug: string;
+  };
+};
+
 // TODO: Add dynamic metadata here.
+export async function generateMetadata({
+  params,
+}: BlogPostParams): Promise<Metadata> {
+  const [project]: { title: string }[] = await sanityClient.fetch(
+    groq`*[_type == "post" && slug.current == "${params.slug}"]{title}`
+  );
+
+  console.log(project);
+
+  return { title: project.title };
+}
 
 export async function generateStaticParams() {
   const blogPosts: BlogPostData[] = await sanityClient.fetch(
@@ -21,13 +39,7 @@ export async function generateStaticParams() {
   });
 }
 
-type BlogPostProps = {
-  params: {
-    slug: string;
-  };
-};
-
-export default async function BlogPost({ params }: BlogPostProps) {
+export default async function BlogPost({ params }: BlogPostParams) {
   const [blogPost]: BlogPostData[] = await sanityClient.fetch(
     groq`*[_type == "post" && slug.current == "${params.slug}"]{title, date, content}`
   );
