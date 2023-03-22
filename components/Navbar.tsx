c6bdf07cc083e6a2fcfe8006ee8fd91727113e86
Brandon <93879641+beanziii@users.noreply.github.com>
2023-03-22 03:31:47 +0000

    removed mobile nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,6 @@
 import clsx from 'clsx';
 import Link from 'next/link';
 import { useSelectedLayoutSegments } from 'next/navigation';
-import { useState } from 'react';
-import { TbMenu2 } from 'react-icons/tb';
-import { RemoveScroll } from 'react-remove-scroll';
 
 const menu = [
   { title: 'About', pathname: '/about' },
@@ -16,26 +13,17 @@ export default function Navbar() {
   const segments = useSelectedLayoutSegments();
 
   return (
-    <>
-      <DesktopMenu segments={segments} />
-      <MobileMenu segments={segments} />
-    </>
-  );
-}
-
-function DesktopMenu({ segments }: { segments: string[] }) {
-  return (
-    <nav className="mx-auto hidden max-w-screen-md items-center justify-between gap-2 text-lg md:flex">
+    <nav className="mx-auto max-w-screen-md items-center justify-between gap-2 flex">
       <Link href="/" className="py-2 tracking-wide">
         Brandon Carlisle
       </Link>
 
-      <ul className="flex gap-2">
+      <ul className="flex gap-[2px] md:gap-2">
         {menu.map((item) => (
           <li key={item.pathname}>
             <Link
               className={clsx(
-                'rounded-md py-2 px-4 font-normal transition-colors hover:bg-blue-900/40',
+                'rounded-md py-2 px-2 md:px-4 font-normal transition-colors hover:bg-blue-900/40 text-sm md:text-base',
                 item.pathname.slice(1) === segments[0] && 'bg-blue-900/30',
               )}
               href={item.pathname}
@@ -46,64 +34,5 @@ function DesktopMenu({ segments }: { segments: string[] }) {
         ))}
       </ul>
     </nav>
-  );
-}
-
-function MobileMenu({ segments }: { segments: string[] }) {
-  const [menuIsOpen, setMenuIsOpen] = useState(false);
-
-  const handleMenuClick = () => {
-    setMenuIsOpen((prev) => !prev);
-  };
-
-  return (
-    <>
-      <button
-        type="button"
-        onClick={handleMenuClick}
-        className="absolute top-4 right-0 z-50 -translate-x-1/2 transition-all active:translate-y-1 md:hidden"
-        aria-label="navigation menu button"
-      >
-        <TbMenu2 className="text-2xl" />
-      </button>
-
-      {!menuIsOpen && (
-        <nav
-          className={
-            'mx-auto flex max-w-screen-md items-center justify-between text-lg md:hidden'
-          }
-        >
-          <Link href="/" className="tracking-wide">
-            Brandon Carlisle
-          </Link>
-        </nav>
-      )}
-
-      {menuIsOpen && (
-        <RemoveScroll>
-          <nav className="absolute inset-0 min-h-screen min-w-full bg-[conic-gradient(at_right,_var(--tw-gradient-stops))] from-slate-900 via-blue-900 to-black p-4 md:hidden">
-            <div className="relative grid min-h-screen min-w-full place-items-center">
-              <ul className="flex flex-col items-center justify-center gap-8">
-                {menu.map((item) => (
-                  <li key={item.pathname}>
-                    <Link
-                      className={clsx(
-                        'rounded-md py-2 px-4 text-2xl font-normal',
-                        item.pathname.slice(1) === segments[0] &&
-                          'bg-blue-900/30',
-                      )}
-                      href={item.pathname}
-                      onClick={handleMenuClick}
-                    >
-                      {item.title}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </nav>
-        </RemoveScroll>
-      )}
-    </>
   );
 }
