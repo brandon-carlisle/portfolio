6d191a112cff886a5651c57f24d49621a82cc44e
beanziii <93879641+beanziii@users.noreply.github.com>
2023-04-02 11:11:37 +0100

    target blank

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -38,7 +38,10 @@ export default function About() {
 
           <p>
             You can also view my CV{' '}
-            <a href="https://docs.google.com/document/d/e/2PACX-1vQE_ijBMvo6JKk7n7Hhk0bR9JwmB6xHMxV56dycpD9gRMR_bPu9nWXc8dQ2y0XkU_L5TuVpIUDKf5vA/pub">
+            <a
+              target="_blank"
+              href="https://docs.google.com/document/d/e/2PACX-1vQE_ijBMvo6JKk7n7Hhk0bR9JwmB6xHMxV56dycpD9gRMR_bPu9nWXc8dQ2y0XkU_L5TuVpIUDKf5vA/pub"
+            >
               here.
             </a>
           </p>
