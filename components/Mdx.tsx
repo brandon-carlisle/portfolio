1eff31cbbf00b3f5a5109d7e30a1f35fd6cc6bd8
beanziii <93879641+beanziii@users.noreply.github.com>
2023-05-25 11:13:29 +0100

    removed width/height not needed

--- components/Mdx.tsx
+++ components/Mdx.tsx
@@ -6,21 +6,10 @@ import Prose from './Prose';
 interface MdxImageProps {
   src: string;
   alt: string;
-  width: number;
-  height: number;
 }
 
-function MdxImage({ src, alt, width, height }: MdxImageProps) {
-  return (
-    <Image
-      src={src}
-      alt={alt}
-      priority
-      placeholder="blur"
-      width={width}
-      height={height}
-    />
-  );
+function MdxImage({ src, alt }: MdxImageProps) {
+  return <Image src={src} alt={alt} priority placeholder="blur" />;
 }
 
 const components = {
