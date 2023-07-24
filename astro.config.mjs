3a9f6b3c89d01f1d6784bdf33ef23c4e12e77ec1
Brandon <93879641+beanziii@users.noreply.github.com>
2023-07-24 15:37:09 +0100

    fix images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  experimental: {
+    assets: true,
+  },
 });
