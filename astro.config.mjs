1a12d6f2211746c6528ec131bd45b27820f49fe7
Brandon <93879641+beanziii@users.noreply.github.com>
2023-07-25 17:49:24 +0100

    seo stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), prefetch()],
   experimental: {
-    assets: true,
-  },
+    assets: true
+  }
 });
